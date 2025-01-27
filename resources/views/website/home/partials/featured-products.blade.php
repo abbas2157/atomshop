f1313c7af735d89a526388b1ca80d1212e5d13a2
Salman <salmanjatt1012@gmail.com>
2025-01-28 01:32:35 +0500

    Product Detail Added

--- resources/views/website/home/partials/featured-products.blade.php
+++ resources/views/website/home/partials/featured-products.blade.php
@@ -7,15 +7,14 @@
                 <div class="col-lg-3 col-md-4 col-sm-6 pb-1">
                     <div class="product-item bg-light mb-4">
                         <div class="product-img position-relative overflow-hidden">
-                            <img class="img-fluid w-100" src="{{ $item->product_picture ?? '' }}" alt="{{ $item->title ?? '' }}">
+                            <img class="img-fluid w-100" style="height: 300px;" src="{{ asset('images/categories/' . $item->picture) }}" alt="{{ $item->title ?? '' }}">
                             <div class="product-action">
                                 <a class="btn btn-outline-dark btn-square" href=""><i class="fa fa-shopping-cart"></i></a>
                                 <a class="btn btn-outline-dark btn-square" href=""><i class="far fa-heart"></i></a>
                             </div>
                         </div>
                         <div class="text-center py-4">
-                            <a class="h6 text-decoration-none text-truncate" href="{{ $item->slug ?? '' }}">{{ $item->title ?? '' }}</a>
-                            <div class="d-flex align-items-center justify-content-center mt-2">
+                            <a class="h6 text-eslint-disable-next-line text-decoration-none text-truncate" href="{{ route('product.detail', ['slug' => $item->slug]) }}">{{ $item->title }}</a>                            <div class="d-flex align-items-center justify-content-center mt-2">
                                 <h5>Rs. {{ $item->formatted_price ?? '' }}</h5><h6 class="text-muted ml-2"><del>Rs. {{ $item->formatted_price ?? '' }}</del></h6>
                             </div>
                             <div class="d-flex align-items-center justify-content-center mb-1">
--- routes/website.php
+++ routes/website.php
@@ -2,3 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 Route::get('/', [App\Http\Controllers\Web\HomeController::class, 'home'])->name('website');
+Route::get('/{slug}', [App\Http\Controllers\Web\HomeController::class, 'product_detail'])->name('product.detail');
