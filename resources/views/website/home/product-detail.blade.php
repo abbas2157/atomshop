929c496a5455602248bbe19aed4a3b00d53e4e70
Salman <salmanjatt1012@gmail.com>
2025-01-28 23:33:31 +0500

    changes

--- resources/views/website/home/product-detail.blade.php
+++ resources/views/website/home/product-detail.blade.php
@@ -8,10 +8,10 @@
         <div class="row px-xl-5">
             <div class="col-12">
                 <nav class="breadcrumb bg-light mb-30">
-                    <a class="breadcrumb-item text-dark" href="{{route('website')}}">Home</a>
-                    <span class="breadcrumb-item active">Product Detail</span>
-                </nav>
-            </div>
+                    <a class="breadcrumb-item text-dark" href="{{ route('website') }}">Home</a>
+                    <span class="breadcrumb-item active">Product</span>
+                    <span> / {{ $product->title ?? '' }}</span>
+                </div>
         </div>
     </div>
 
@@ -22,7 +22,7 @@
             <div class="col-lg-5 mb-30">
                 <div id="product-carousel" class="carousel slide" data-ride="carousel">
                     <div class="carousel-inner bg-light">
-                        @foreach($productImages as $index => $image)
+                        @foreach ($productImages as $index => $image)
                             <div class="carousel-item {{ $index == 0 ? 'active' : '' }}">
                                 <img class="w-100 h-100" src="{{ asset($image->url ?? '') }}" alt="Image">
                             </div>
@@ -48,13 +48,13 @@
                             <small class="fas fa-star-half-alt"></small>
                             <small class="far fa-star"></small>
                         </div>
-                        <small class="pt-1">(99 Reviews)</small>
+                        <small class="pt-1">(00 Reviews)</small>
                     </div>
-                    <h3 class="font-weight-semi-bold mb-4">${{ $product->price ?? '' }}</h3>
+                    <h3 class="font-weight-semi-bold mb-4">RS/-{{ $product->price ?? '' }}</h3>
                     <p class="mb-4">{{ $description->short ?? '' }}</p>
                     <div class="d-flex mb-3">
-                        <strong class="text-dark mr-3">Sizes:</strong>
-                        <form>
+                        <strong class="text-dark mr-3">{{ $memory->title ?? '' }}:</strong>
+                        {{-- <form>
                             <div class="custom-control custom-radio custom-control-inline">
                                 <input type="radio" class="custom-control-input" id="size-1" name="size">
                                 <label class="custom-control-label" for="size-1">XS</label>
@@ -75,16 +75,15 @@
                                 <input type="radio" class="custom-control-input" id="size-5" name="size">
                                 <label class="custom-control-label" for="size-5">XL</label>
                             </div>
-                        </form>
+                        </form> --}}
                     </div>
                     <div class="d-flex mb-4">
-                        <strong class="text-dark mr-3">Colors:</strong>
+                        <strong class="text-dark mr-3">Color:</strong>
                         <form>
-                            <div class="custom-control custom-radio custom-control-inline">
-                                <input type="radio" class="custom-control-input" id="color-1" name="color">
-                                <label class="custom-control-label" for="color-1">Black</label>
+                            <div class="">
+                                {{ $color->title ?? '' }}
                             </div>
-                            <div class="custom-control custom-radio custom-control-inline">
+                            {{-- <div class="custom-control custom-radio custom-control-inline">
                                 <input type="radio" class="custom-control-input" id="color-2" name="color">
                                 <label class="custom-control-label" for="color-2">White</label>
                             </div>
@@ -99,7 +98,7 @@
                             <div class="custom-control custom-radio custom-control-inline">
                                 <input type="radio" class="custom-control-input" id="color-5" name="color">
                                 <label class="custom-control-label" for="color-5">Green</label>
-                            </div>
+                            </div> --}}
                         </form>
                     </div>
                     <div class="d-flex align-items-center mb-4 pt-2">
@@ -119,23 +118,6 @@
                         <button class="btn btn-primary px-3"><i class="fa fa-shopping-cart mr-1"></i> Add To
                             Cart</button>
                     </div>
-                    <div class="d-flex pt-2">
-                        <strong class="text-dark mr-2">Share on:</strong>
-                        <div class="d-inline-flex">
-                            <a class="text-dark px-2" href="">
-                                <i class="fab fa-facebook-f"></i>
-                            </a>
-                            <a class="text-dark px-2" href="">
-                                <i class="fab fa-twitter"></i>
-                            </a>
-                            <a class="text-dark px-2" href="">
-                                <i class="fab fa-linkedin-in"></i>
-                            </a>
-                            <a class="text-dark px-2" href="">
-                                <i class="fab fa-pinterest"></i>
-                            </a>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
@@ -145,57 +127,12 @@
                     <div class="nav nav-tabs mb-4">
                         <a class="nav-item nav-link text-dark active" data-toggle="tab"
                             href="#tab-pane-1">Description</a>
-                        <a class="nav-item nav-link text-dark" data-toggle="tab" href="#tab-pane-2">Information</a>
                         <a class="nav-item nav-link text-dark" data-toggle="tab" href="#tab-pane-3">Reviews (0)</a>
                     </div>
                     <div class="tab-content">
                         <div class="tab-pane fade show active" id="tab-pane-1">
                             <h4 class="mb-3">Product Description</h4>
                             <p>{!! $description->long ?? '' !!}</p>
-                        </div>
-                        <div class="tab-pane fade" id="tab-pane-2">
-                            <h4 class="mb-3">Additional Information</h4>
-                            <p>Eos no lorem eirmod diam diam, eos elitr et gubergren diam sea. Consetetur vero aliquyam
-                                invidunt duo dolores et duo sit. Vero diam ea vero et dolore rebum, dolor rebum eirmod
-                                consetetur invidunt sed sed et, lorem duo et eos elitr, sadipscing kasd ipsum rebum diam.
-                                Dolore diam stet rebum sed tempor kasd eirmod. Takimata kasd ipsum accusam sadipscing, eos
-                                dolores sit no ut diam consetetur duo justo est, sit sanctus diam tempor aliquyam eirmod
-                                nonumy rebum dolor accusam, ipsum kasd eos consetetur at sit rebum, diam kasd invidunt
-                                tempor lorem, ipsum lorem elitr sanctus eirmod takimata dolor ea invidunt.</p>
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <ul class="list-group list-group-flush">
-                                        <li class="list-group-item px-0">
-                                            Sit erat duo lorem duo ea consetetur, et eirmod takimata.
-                                        </li>
-                                        <li class="list-group-item px-0">
-                                            Amet kasd gubergren sit sanctus et lorem eos sadipscing at.
-                                        </li>
-                                        <li class="list-group-item px-0">
-                                            Duo amet accusam eirmod nonumy stet et et stet eirmod.
-                                        </li>
-                                        <li class="list-group-item px-0">
-                                            Takimata ea clita labore amet ipsum erat justo voluptua. Nonumy.
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div class="col-md-6">
-                                    <ul class="list-group list-group-flush">
-                                        <li class="list-group-item px-0">
-                                            Sit erat duo lorem duo ea consetetur, et eirmod takimata.
-                                        </li>
-                                        <li class="list-group-item px-0">
-                                            Amet kasd gubergren sit sanctus et lorem eos sadipscing at.
-                                        </li>
-                                        <li class="list-group-item px-0">
-                                            Duo amet accusam eirmod nonumy stet et et stet eirmod.
-                                        </li>
-                                        <li class="list-group-item px-0">
-                                            Takimata ea clita labore amet ipsum erat justo voluptua. Nonumy.
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
                         </div>
                         <div class="tab-pane fade" id="tab-pane-3">
                             <div class="row">
@@ -266,10 +203,12 @@
         <div class="row px-xl-5">
             <div class="col">
                 <div class="owl-carousel related-carousel">
-                    @foreach($feature_products as $item)
+                    @foreach ($feature_products as $item)
                         <div class="product-item bg-light">
                             <div class="product-img position-relative overflow-hidden">
-                                <img class="img-fluid w-100"  style="height: 300px;" src="{{ asset('images/categories/' . $item->picture) }}" alt="{{ $item->title ?? '' }}">
+                                <img class="img-fluid w-100" style="height: 300px;"
+                                    src="{{ asset('images/categories/' . $item->picture) }}"
+                                    alt="{{ $item->title ?? '' }}">
                                 <div class="product-action">
                                     <a class="btn btn-outline-dark btn-square" href=""><i
                                             class="fa fa-shopping-cart"></i></a>
@@ -282,7 +221,8 @@
                                 </div>
                             </div>
                             <div class="text-center py-4">
-                                <a class="h6 text-decoration-none text-truncate" href="{{ route('product.detail', ['slug' => $item->slug]) }}">{{ $item->title ?? '' }}</a>
+                                <a class="h6 text-decoration-none text-truncate"
+                                    href="{{ route('product.detail', ['slug' => $item->slug]) }}">{{ $item->title ?? '' }}</a>
                                 <div class="d-flex align-items-center justify-content-center mt-2">
                                     <h5>RS. {{ $item->formatted_price ?? '' }}</h5>
                                     <h6 class="text-muted ml-2"><del>RS. {{ $item->formatted_price ?? '' }}</del></h6>
