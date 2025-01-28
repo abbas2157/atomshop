4ac567f415630817c1d48bab8b032ae7b4153326
Mudassar Abbas <mudassar@zoomdigital.ae>
2025-01-28 17:17:08 +0500

    Website HOme Page setting

--- resources/views/dashboards/admin/components/products/edit.blade.php
+++ resources/views/dashboards/admin/components/products/edit.blade.php
@@ -74,41 +74,36 @@
                         var colorId = $('#color_id').parsley();
                         var status = $('#status').parsley();
 
-                            if (
-                                title.isValid() &&
-                                categoryId.isValid() &&
-                                brandId.isValid() &&
-                                memoryId.isValid() &&
-                                colorId.isValid() &&
-                                status.isValid()
-                            ) {
-                                return true;
-                            } else {
-                                title.validate();
-                                categoryId.validate();
-                                brandId.validate();
-                                memoryId.validate();
-                                colorId.validate();
-                                status.validate();
-                            }
-                        } else if (currentIndex === 1) {
-                            var short = $('#short_description').parsley();
-                            if (short.isValid()) {
-                                return true;
-                            } else {
-                                short.validate();
-                            }
-                        } else if (currentIndex === 2) {
+                        if (title.isValid() && categoryId.isValid() && brandId.isValid() && memoryId.isValid() && colorId.isValid() && status.isValid()) {
                             return true;
-                            var picture = $('#picture').parsley();
-                            if (picture.isValid()) {
-                                return true;
-                            } else {
-                                picture.validate();
-                            }
+                        } 
+                        else {
+                            title.validate();
+                            categoryId.validate();
+                            brandId.validate();
+                            memoryId.validate();
+                            colorId.validate();
+                            status.validate();
                         }
-                    } else {
+                    } 
+                    else if (currentIndex === 1) {
+                        var short = $('#short_description').parsley();
+                        if (short.isValid()) {
+                            return true;
+                        } 
+                        else {
+                            short.validate();
+                        }
+                    } 
+                    else if (currentIndex === 2) {
                         return true;
+                        var picture = $('#picture').parsley();
+                        if (picture.isValid()) {
+                            return true;
+                        } 
+                        else {
+                            picture.validate();
+                        }
                     }
                 } 
                 else {
@@ -117,38 +112,37 @@
             },
             onFinishing: function (event, currentIndex) {
                 if (isSubmitting) {
-                        return false;
-                    }
-                    isSubmitting = true;
+                    return false;
+                }
+                isSubmitting = true;
 
                 $('.actions a[href="#finish"]').text('Publishing...').addClass('disabled').css('pointer-events', 'none');
                 console.log('Finishing... Current Index:', currentIndex);
 
-                    var productId = '{{ $product->id }}';
-                    var formData = new FormData(document.getElementById('product-form-name'));
+                var productId = '{{ $product->id }}';
+                var formData = new FormData(document.getElementById('product-form-name'));
 
-                    $.ajax({
-                        url: "{{ route('admin.products.update', $product->id) }}",
-                        type: 'POST',
-                        data: formData,
-                        contentType: false,
-                        processData: false,
-                        headers: {
-                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content'),
-                            'X-HTTP-Method-Override': 'PUT'
-                        },
-                        success: function(response) {
-                            console.log(response);
-                            location.reload();
-                        },
-                        error: function(xhr, status, error) {
-                            alert('An error occurred. Please try again.');
-                            console.error(xhr.responseText);
-                        }
-                    });
-                }
-            });
-
+                $.ajax({
+                    url: "{{ route('admin.products.update', $product->id) }}",
+                    type: 'POST',
+                    data: formData,
+                    contentType: false,
+                    processData: false,
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content'),
+                        'X-HTTP-Method-Override': 'PUT'
+                    },
+                    success: function(response) {
+                        console.log(response);
+                        location.reload();
+                    },
+                    error: function(xhr, status, error) {
+                        alert('An error occurred. Please try again.');
+                        console.error(xhr.responseText);
+                    }
+                });
+            }
+        });
             $('#title').attr('data-parsley-required', 'true');
             $('#category_id').attr('data-parsley-required', 'true');
             $('#brand_id').attr('data-parsley-required', 'true');
@@ -213,87 +207,33 @@
             ];
         });
 
-        function deleteGalleryImage(productId, imageId) {
-            if (confirm('Are you sure you want to delete this gallery image?')) {
-                $.ajax({
-                    type: 'DELETE',
-                    url: '{{ route('products.gallery-image', [$product->id, ':imageId']) }}'.replace(
-                        ':imageId', imageId),
-                    data: {
-                        _token: '{{ csrf_token() }}',
-                        id: imageId
-                    },
-                    success: function(response) {
-                        if (response.success) {
-                            location.reload();
-                        } else {
-                            alert(response.message);
+            function deleteGalleryImage(productId, imageId) {
+                if (confirm('Are you sure you want to delete this gallery image?')) {
+                    $.ajax({
+                        type: 'DELETE',
+                        url: '{{ route('products.gallery-image', [$product->id, ':imageId']) }}'.replace(
+                            ':imageId', imageId),
+                        data: {
+                            _token: '{{ csrf_token() }}',
+                            id: imageId
+                        },
+                        success: function(response) {
+                            if (response.success) {
+                                location.reload();
+                            } else {
+                                alert(response.message);
+                            }
+                        },
+                        error: function (xhr, status, error) {
+                            alert('An error occurred. Please try again.');
+                            console.error(xhr.responseText);
+                            isSubmitting = false;
+                            
+                            $('.actions a[href="#finish"]').text('Publish Product').removeClass('disabled').css('pointer-events', 'auto');
                         }
-                    },
-                    error: function (xhr, status, error) {
-                        alert('An error occurred. Please try again.');
-                        console.error(xhr.responseText);
-                        isSubmitting = false;
-                        
-                        $('.actions a[href="#finish"]').text('Publish Product').removeClass('disabled').css('pointer-events', 'auto');
-                    }
-                });
+                    });
+                }
             }
-        });
-        
-        $('#title').attr('data-parsley-required', 'true');
-        $('#category_id').attr('data-parsley-required', 'true');
-        $('#brand_id').attr('data-parsley-required', 'true');
-        $('#memory_id').attr('data-parsley-required', 'true');
-        $('#color_id').attr('data-parsley-required', 'true');
-        $('#status').attr('data-parsley-required', 'true');
-        $('#customFile').attr('data-parsley-required', 'true');
-        $('#product-form form').parsley();
-
-        $('.select2').select2({
-            placeholder: 'Choose items',
-            searchInputPlaceholder: 'Search'
-        });
-
-        var icons = Quill.import('ui/icons');
-        icons['bold'] = '<i class="la la-bold" aria-hidden="true"></i>';
-        icons['italic'] = '<i class="la la-italic" aria-hidden="true"></i>';
-        icons['underline'] = '<i class="la la-underline" aria-hidden="true"></i>';
-        icons['strike'] = '<i class="la la-strikethrough" aria-hidden="true"></i>';
-        icons['list']['ordered'] = '<i class="la la-list-ol" aria-hidden="true"></i>';
-        icons['list']['bullet'] = '<i class="la la-list-ul" aria-hidden="true"></i>';
-        icons['link'] = '<i class="la la-link" aria-hidden="true"></i>';
-        icons['image'] = '<i class="la la-image" aria-hidden="true"></i>';
-        icons['video'] = '<i class="la la-film" aria-hidden="true"></i>';
-        icons['code-block'] = '<i class="la la-code" aria-hidden="true"></i>';
-
-        var toolbarOptions = [
-            [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-            ['bold', 'italic', 'underline', 'strike'],
-            [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-            ['link', 'image', 'video']
-        ];
-
-        var quill = new Quill('#londDescription', {
-            modules: {
-                toolbar: toolbarOptions
-            },
-            theme: 'snow'
-        });
-
-        var initialContent = @json(old('long', $productdescription->long));
-        quill.clipboard.dangerouslyPasteHTML(initialContent);
-
-        quill.on('text-change', function () {
-            var htmlContent = quill.root.innerHTML;
-            document.getElementById('long').value = htmlContent;
-        });
-        var toolbarInlineOptions = [
-            ['bold', 'italic', 'underline'],
-            [{ 'header': 1 }, { 'header': 2 }, 'blockquote'],
-            ['link', 'image', 'code-block'],
-        ];
-    });
 </script>
 
 <script>
