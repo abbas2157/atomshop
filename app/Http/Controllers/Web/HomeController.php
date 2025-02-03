4fe310fd6ba769809a7540c421a7a1e8729eb092
Salman <salmanjatt1012@gmail.com>
2025-02-03 23:09:57 +0500

    Category Images

--- app/Http/Controllers/Web/HomeController.php
+++ app/Http/Controllers/Web/HomeController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\{Category, WebsiteSetup};
+use App\Models\{Product, WebsiteSetup};
 
 class HomeController extends Controller
 {
