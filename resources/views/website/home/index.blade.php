a26b59a772b0592d35e9ecae46a3e1af4087333f
Mudassar Abbas <mudassar@zoomdigital.ae>
2025-02-04 13:33:59 +0500

    Home page Setting

--- resources/views/website/home/index.blade.php
+++ resources/views/website/home/index.blade.php
@@ -4,6 +4,7 @@
     <meta content="Atomshop - Pay in steps" name="description">
 @endsection
 @section('content')
+    @include('website.home.partials.sliders')
     @include('website.home.partials.featured-start')
     @include('website.home.partials.categories')
     @include('website.home.partials.featured-products')
