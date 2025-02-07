f7fadd0e1c5cb90ac003d0d06f1baad58eaa58a3
Maryam Saleem <maryamsaleem108@gmail.com>
2025-02-07 10:33:13 +0500

    sliders side bar show

--- resources/views/dashboards/admin/web-app/website/partials/sidebar.blade.php
+++ resources/views/dashboards/admin/web-app/website/partials/sidebar.blade.php
@@ -10,5 +10,10 @@
             <a href="{{ route('admin.website.categories') }}" class="nav-link {{ (request()->segment(2) == 'website' && (request()->segment(3) == 'categories')) ? 'active' : '' }}">Categories Section</a>
             <a href="{{ route('admin.website.brands') }}" class="nav-link {{ (request()->segment(2) == 'website' && (request()->segment(3) == 'brands')) ? 'active' : '' }}">Brands Section</a>
         </nav>
+
+        <label>Sliders</label>
+        <nav class="nav flex-column">
+            <a href="{{ route('admin.website.sliders') }}" class="nav-link {{ (request()->segment(2) == 'website' && (request()->segment(3) == 'sliders') && (request()->segment(4) == 'feature')) ? 'active' : '' }}">Sliders Section</a>
+        </nav>
     </div>
 </div>
