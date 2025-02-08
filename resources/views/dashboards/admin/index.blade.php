8733f750289ec6d992aca1dc2fe3ce246311a3a1
Salman <salmanjatt1012@gmail.com>
2025-02-08 23:55:38 +0500

    admin dashboard

--- resources/views/dashboards/admin/index.blade.php
+++ resources/views/dashboards/admin/index.blade.php
@@ -1,6 +1,6 @@
 @extends('dashboards.admin.layout.app')
 @section('title')
-    <title>Admin Dashboard - {{ env('APP_NAME') ?? 'AtomShop' }}</title> 
+    <title>Admin Dashboard - {{ env('APP_NAME') ?? 'AtomShop' }}</title>
 @endsection
 @section('content')
 <div class="az-content az-content-dashboard">
@@ -16,218 +16,45 @@
                     <h5>00,00</h5>
                 </div>
             </div>
-            <div class="card card-body card-dashboard-twentyfive mg-b-20">
-              <h6 class="card-title">Conversions</h6>
-              <div class="row row-sm">
-                <div class="col-6 col-sm-4 col-lg">
-                  <label class="card-label">Conversion Rate</label>
-                  <h6 class="card-value">0.81<small>%</small></h6>
-                  <div class="chart-wrapper">
-                    <div id="flotChart1" class="flot-chart" style="padding: 0px; position: relative;"><canvas class="flot-base" style="direction: ltr; position: absolute; left: 0px; top: 0px; width: 166.664px; height: 35px;" width="333" height="70"></canvas><canvas class="flot-overlay" style="direction: ltr; position: absolute; left: 0px; top: 0px; width: 166.664px; height: 35px;" width="333" height="70"></canvas></div>
-                  </div><!-- chart-wrapper -->
-                </div><!-- col -->
-                <div class="col-6 col-sm-4 col-lg">
-                  <label class="card-label">Revenue</label>
-                  <h6 class="card-value"><span>$</span>1,095,190</h6>
-                  <div class="chart-wrapper">
-                    <div id="flotChart2" class="flot-chart" style="padding: 0px; position: relative;"><canvas class="flot-base" style="direction: ltr; position: absolute; left: 0px; top: 0px; width: 166.664px; height: 35px;" width="333" height="70"></canvas><canvas class="flot-overlay" style="direction: ltr; position: absolute; left: 0px; top: 0px; width: 166.664px; height: 35px;" width="333" height="70"></canvas></div>
-                  </div><!-- chart-wrapper -->
-                </div><!-- col -->
-                <div class="col-6 col-sm-4 col-lg mg-t-20 mg-sm-t-0">
-                  <label class="card-label">Unique Purchases</label>
-                  <h6 class="card-value">53</h6>
-                  <div class="chart-wrapper">
-                    <div id="flotChart3" class="flot-chart" style="padding: 0px; position: relative;"><canvas class="flot-base" style="direction: ltr; position: absolute; left: 0px; top: 0px; width: 166.664px; height: 35px;" width="333" height="70"></canvas><canvas class="flot-overlay" style="direction: ltr; position: absolute; left: 0px; top: 0px; width: 166.664px; height: 35px;" width="333" height="70"></canvas></div>
-                  </div><!-- chart-wrapper -->
-                </div><!-- col -->
-                <div class="col-6 col-sm-4 col-lg mg-t-20 mg-lg-t-0">
-                  <label class="card-label">Transactions</label>
-                  <h6 class="card-value">31</h6>
-                  <div class="chart-wrapper">
-                    <div id="flotChart4" class="flot-chart" style="padding: 0px; position: relative;"><canvas class="flot-base" style="direction: ltr; position: absolute; left: 0px; top: 0px; width: 166.664px; height: 35px;" width="333" height="70"></canvas><canvas class="flot-overlay" style="direction: ltr; position: absolute; left: 0px; top: 0px; width: 166.664px; height: 35px;" width="333" height="70"></canvas></div>
-                  </div><!-- chart-wrapper -->
-                </div><!-- col -->
-                <div class="col-6 col-sm-4 col-lg mg-t-20 mg-lg-t-0">
-                  <label class="card-label">Avg. Order Value</label>
-                  <h6 class="card-value"><span>$</span>306.20</h6>
-                  <div class="chart-wrapper">
-                    <div id="flotChart5" class="flot-chart" style="padding: 0px; position: relative;"><canvas class="flot-base" style="direction: ltr; position: absolute; left: 0px; top: 0px; width: 166.664px; height: 35px;" width="333" height="70"></canvas><canvas class="flot-overlay" style="direction: ltr; position: absolute; left: 0px; top: 0px; width: 166.664px; height: 35px;" width="333" height="70"></canvas></div>
-                  </div><!-- chart-wrapper -->
-                </div><!-- col -->
-                <div class="col-6 col-sm-4 col-lg mg-t-20 mg-lg-t-0">
-                  <label class="card-label">Quantity</label>
-                  <h6 class="card-value">52</h6>
-                  <div class="chart-wrapper">
-                    <div id="flotChart6" class="flot-chart" style="padding: 0px; position: relative;"><canvas class="flot-base" style="direction: ltr; position: absolute; left: 0px; top: 0px; width: 166.664px; height: 35px;" width="333" height="70"></canvas><canvas class="flot-overlay" style="direction: ltr; position: absolute; left: 0px; top: 0px; width: 166.664px; height: 35px;" width="333" height="70"></canvas></div>
-                  </div><!-- chart-wrapper -->
-                </div><!-- col -->
-              </div><!-- row -->
-            </div>
-            <div class="card card-dashboard-seven">
-                <div class="card-header">
-                  <div class="row row-sm">
-                    <div class="col-6 col-md-4 col-xl">
-                      <div class="media">
-                        <div><i class="icon ion-ios-calendar"></i></div>
-                        <div class="media-body">
-                          <label>Start Date</label>
-                          <div class="date">
-                            <span>Sept 01, 2018</span> <a href=""><i class="icon ion-md-arrow-dropdown"></i></a>
-                          </div>
-                        </div>
-                      </div><!-- media -->
-                    </div>
-                    <div class="col-6 col-md-4 col-xl">
-                      <div class="media">
-                        <div><i class="icon ion-ios-calendar"></i></div>
-                        <div class="media-body">
-                          <label>End Date</label>
-                          <div class="date">
-                            <span>Sept 30, 2018</span> <a href=""><i class="icon ion-md-arrow-dropdown"></i></a>
-                          </div>
-                        </div>
-                      </div><!-- media -->
-                    </div>
-                    <div class="col-6 col-md-4 col-xl mg-t-15 mg-md-t-0">
-                      <div class="media">
-                        <div><i class="icon ion-logo-usd"></i></div>
-                        <div class="media-body">
-                          <label>Sales Measure</label>
-                          <div class="date">
-                            <span>Revenue</span> <a href=""><i class="icon ion-md-arrow-dropdown"></i></a>
-                          </div>
-                        </div>
-                      </div><!-- media -->
-                    </div>
-                    <div class="col-6 col-md-4 col-xl mg-t-15 mg-xl-t-0">
-                      <div class="media">
-                        <div><i class="icon ion-md-person"></i></div>
-                        <div class="media-body">
-                          <label>Customer Type</label>
-                          <div class="date">
-                            <span>All Customers</span> <a href=""><i class="icon ion-md-arrow-dropdown"></i></a>
-                          </div>
-                        </div>
-                      </div><!-- media -->
-                    </div>
-                    <div class="col-md-4 col-xl mg-t-15 mg-xl-t-0">
-                      <div class="media">
-                        <div><i class="icon ion-md-stats"></i></div>
-                        <div class="media-body">
-                          <label>Transaction Type</label>
-                          <div class="date">
-                            <span>All Transactions</span> <a href=""><i class="icon ion-md-arrow-dropdown"></i></a>
-                          </div>
-                        </div>
-                      </div><!-- media -->
-                    </div>
-                  </div><!-- row -->
-                </div><!-- card-header -->
-                <div class="card-body">
-                  <div class="row row-sm">
-                    <div class="col-6 col-lg-3">
-                      <label class="az-content-label">Total Quantity</label>
-                      <h2>00,00</h2>
-                      <div class="desc up">
-                        <i class="icon ion-md-stats"></i>
-                        <span><strong>0.00%</strong> (30 days)</span>
-                      </div>
-                    </div><!-- col -->
-                    <div class="col-6 col-lg-3">
-                      <label class="az-content-label">Total Cost</label>
-                      <h2><span>Rs. </span>00,00</h2>
-                      <div class="desc up">
-                        <i class="icon ion-md-stats"></i>
-                        <span><strong>0.00%</strong> (30 days)</span>
-                      </div>
-                    </div><!-- col -->
-                    <div class="col-6 col-lg-3 mg-t-20 mg-lg-t-0">
-                      <label class="az-content-label">Total Revenue</label>
-                      <h2><span>Rs. </span>00,00</h2>
-                      <div class="desc down">
-                        <i class="icon ion-md-stats"></i>
-                        <span><strong>0.00%</strong> (30 days)</span>
-                      </div>
-                    </div><!-- col -->
-                    <div class="col-6 col-lg-3 mg-t-20 mg-lg-t-0">
-                      <label class="az-content-label">Total Profit</label>
-                      <h2><span>Rs. </span>00,00</h2>
-                      <div class="desc up">
-                        <i class="icon ion-md-stats"></i>
-                        <span><strong>0.00%</strong> (30 days)</span>
-                      </div>
-                    </div><!-- col -->
-                  </div><!-- row -->
-                </div><!-- card-body -->
-            </div>
+
             <div class="row row-sm">
-                <div class="col-sm-6 col-lg-3">
-                  <div class="card card-dashboard-donut">
-                    <div class="card-header">
-                      <h6 class="card-title mg-b-10">Gross Profit Margin</h6>
-                      <p class="mg-b-0 tx-12 tx-gray-500">The profit you make on each dollar of sales... <a href="">Learn more</a></p>
-                    </div><!-- card-header -->
-                    <div class="card-body">
-                      <div class="az-donut-chart chart1">
-                        <div class="slice one"></div>
-                        <div class="slice two"></div>
-                        <div class="chart-center">
-                          <span></span>
-                        </div>
-                      </div>
-                    </div><!-- card-body -->
-                  </div><!-- card -->
-                </div>
-                <div class="col-sm-6 col-lg-3 mg-t-20 mg-sm-t-0">
-                  <div class="card card-dashboard-donut">
-                    <div class="card-header">
-                      <h6 class="card-title mg-b-10">Net Profit Margin</h6>
-                      <p class="mg-b-0 tx-12 tx-gray-500">Measures your business at generating prof... <a href="">Learn more</a></p>
-                    </div><!-- card-header -->
-                    <div class="card-body">
-                      <div class="az-donut-chart chart2">
-                        <div class="slice one"></div>
-                        <div class="slice two"></div>
-                        <div class="chart-center">
-                          <span></span>
-                        </div>
-                      </div>
-                    </div><!-- card-body -->
-                  </div><!-- card -->
-                </div>
-                <div class="col-lg-6">
+                <div class="col-lg-12">
                     <div class="row row-sm">
-                      <div class="col-sm-6">
-                        <div class="card card-dashboard-finance">
-                          <h6 class="card-title">Total Income</h6>
-                          <h2><span>Rs. </span>00,00<small>.00</small></h2>
-                          <span class="tx-12"><span class="tx-success tx-bold">0.0%</span> higher vs previous month</span>
-                        </div>
-                      </div><!-- col -->
-                      <div class="col-sm-6 mg-t-20 mg-sm-t-0">
-                        <div class="card card-dashboard-finance">
-                          <h6 class="card-title">Total Expenses</h6>
-                          <h2><span>Rs. </span>00,00<small>.00</small></h2>
-                          <span class="tx-12"><span class="tx-danger tx-bold">0.0%</span> higher vs previous month</span>
-                        </div>
-                      </div><!-- col -->
-                      <div class="col-sm-6 mg-t-20">
-                        <div class="card card-dashboard-finance">
-                          <h6 class="card-title">Accounts Receivable</h6>
-                          <h2><span>Rs. </span>00,00<small>.00</small></h2>
-                          <span class="tx-12"><span class="tx-success tx-bold">0.0%</span> higher vs previous month</span>
-                        </div>
-                      </div><!-- col -->
-                      <div class="col-sm-6 mg-t-20">
-                        <div class="card card-dashboard-finance">
-                          <h6 class="card-title">Accounts Payable</h6>
-                          <h2><span>$</span>8,216<small>.00</small></h2>
-                          <span class="tx-12"><span class="tx-success tx-bold">0.7%</span> higher vs previous month</span>
-                        </div>
-                      </div><!-- col -->
+                        <div class="col-md-3">
+                            <div class="card card-dashboard-finance">
+                                <h6 class="card-title">Total Seller</h6>
+                                <h2><span>Rs. </span>00,00<small>.00</small></h2>
+                                <span class="tx-12"><span class="tx-success tx-bold">0.0%</span> higher vs previous month</span>
+                            </div>
+                        </div><!-- col -->
+
+                        <div class="col-md-3">
+                            <div class="card card-dashboard-finance">
+                                <h6 class="card-title">Verified Seller</h6>
+                                <h2><span>Rs. </span>00,00<small>.00</small></h2>
+                                <span class="tx-12"><span class="tx-danger tx-bold">0.0%</span> higher vs previous month</span>
+                            </div>
+                        </div><!-- col -->
+
+                        <div class="col-md-3">
+                            <div class="card card-dashboard-finance">
+                                <h6 class="card-title">Total customer</h6>
+                                <h2><span>Rs. </span>00,00<small>.00</small></h2>
+                                <span class="tx-12"><span class="tx-success tx-bold">0.0%</span> higher vs previous month</span>
+                            </div>
+                        </div><!-- col -->
+
+                        <div class="col-md-3">
+                            <div class="card card-dashboard-finance">
+                                <h6 class="card-title">Verified Customer</h6>
+                                <h2><span>$</span>8,216<small>.00</small></h2>
+                                <span class="tx-12"><span class="tx-success tx-bold">0.7%</span> higher vs previous month</span>
+                            </div>
+                        </div><!-- col -->
                     </div><!-- row -->
-                </div>
-            </div>
+                </div><!-- col-lg-12 -->
+            </div><!-- row -->
+
         </div>
     </div>
 </div>
