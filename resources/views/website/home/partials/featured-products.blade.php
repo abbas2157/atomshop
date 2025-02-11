71ae12ca843f59e9d95cad707a1a0ab421155aeb
Mudassar Abbas <mudassar@zoomdigital.ae>
2025-02-11 14:12:33 +0500

    Product CHanges

--- resources/views/website/home/partials/featured-products.blade.php
+++ resources/views/website/home/partials/featured-products.blade.php
@@ -4,7 +4,7 @@
         <h2 class="section-title position-relative text-uppercase mx-xl-5 mb-4"><span class="bg-secondary pr-3">Featured Products</span></h2>
         <div class="row px-xl-5">
             @foreach($feature_products as $item)
-                <div class="col-lg-2 col-md-4 col-sm-6 pb-1">
+                <div class="col-lg-3 col-md-4 col-sm-6 pb-1">
                     <div class="product-item bg-light mb-2">
                         <div class="product-img position-relative overflow-hidden text-center py-3">
                             <img class="img-fluid w-75" src="{{  $item->picture ?? '' }}" alt="{{ $item->title ?? '' }}">
@@ -21,7 +21,7 @@
                             </div>
                             <div class="row">
                                 <div class="col-lg-8 col-md-8 col-sm-8 pb-1">
-                                    <div class="product-item-price">Advance : <b>{{ $item->price ?? '00.00' }}</b></div>
+                                    <div class="product-item-price">Advance : Rs. <b>{{ $item->price ?? '00.00' }}</b></div>
                                 </div>
                                 <div class="col-lg-4 col-md-4 col-sm-4 text-left">
                                     <div class="product-item-brand "><b>{{ $item->brand ?? '' }}</b></div>
