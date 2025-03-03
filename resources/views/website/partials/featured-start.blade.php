58b3e31fb35a3d83ef904e5ea7c8a96c4e5698ce
Mudassar Abbas <mudassar@zoomdigital.ae>
2025-03-03 15:55:55 +0500

    Calcualtor Content

--- resources/views/website/partials/featured-start.blade.php
+++ resources/views/website/partials/featured-start.blade.php
@@ -1,28 +1,28 @@
 <!-- Featured Start -->
-<div class="container-fluid pt-5">
-    <div class="row px-xl-5 pb-3">
+<div class="container-fluid">
+    <div class="row px-xl-5">
         <div class="col-lg-3 col-md-6 col-sm-12 pb-1">
             <div class="d-flex align-items-center bg-light mb-4 p-30" >
-                <h1 class="fa fa-check text-primary m-0 mr-3"></h1>
-                <h5 class="font-weight-semi-bold m-0">Quality Product</h5>
+                <h1 class="fa fa-receipt text-primary m-0 mr-3"></h1>
+                <h5 class="font-weight-semi-bold m-0">Custom Installment <br /> Plans</h5>
             </div>
         </div>
         <div class="col-lg-3 col-md-6 col-sm-12 pb-1">
             <div class="d-flex align-items-center bg-light mb-4 p-30" >
-                <h1 class="fa fa-shipping-fast text-primary m-0 mr-2"></h1>
-                <h5 class="font-weight-semi-bold m-0">Free Shipping</h5>
+                <h1 class="fa fa-phone-volume text-primary m-0 mr-2"></h1>
+                <h5 class="font-weight-semi-bold m-0">Easy Verification <br /> Process</h5>
             </div>
         </div>
         <div class="col-lg-3 col-md-6 col-sm-12 pb-1">
             <div class="d-flex align-items-center bg-light mb-4 p-30" >
-                <h1 class="fas fa-exchange-alt text-primary m-0 mr-3"></h1>
-                <h5 class="font-weight-semi-bold m-0">14-Day Return</h5>
+                <h1 class="fas fa-shipping-fast text-primary m-0 mr-3"></h1>
+                <h5 class="font-weight-semi-bold m-0">Door-Step <br /> Services</h5>
             </div>
         </div>
         <div class="col-lg-3 col-md-6 col-sm-12 pb-1">
             <div class="d-flex align-items-center bg-light mb-4 p-30" >
-                <h1 class="fa fa-phone-volume text-primary m-0 mr-3"></h1>
-                <h5 class="font-weight-semi-bold m-0">24/7 Support</h5>
+                <h1 class="fa fa-exchange-alt text-primary m-0 mr-3"></h1>
+                <h5 class="font-weight-semi-bold m-0">Any Product, <br /> Sourced!</h5>
             </div>
         </div>
     </div>
