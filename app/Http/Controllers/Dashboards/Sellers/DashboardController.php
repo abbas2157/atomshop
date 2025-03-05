f5e8e4bb3374638615680a72ab17bdc9229a8d12
Maryam Saleem <maryamsaleem108@gmail.com>
2025-03-05 21:10:11 +0500

    seller dashboard changes

--- app/Http/Controllers/Dashboards/Sellers/DashboardController.php
+++ app/Http/Controllers/Dashboards/Sellers/DashboardController.php
@@ -4,15 +4,66 @@ namespace App\Http\Controllers\Dashboards\Sellers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\{User, Customer, Seller, Order};
+use Illuminate\Support\Facades\{Auth, DB, Password, Hash, Mail};
+
 
 class DashboardController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+   
+     public function index()
     {
-        return view('dashboards.sellers.index');
+        $authUser = Auth::user();
+        $data = [];
+
+        if ($authUser->seller) {
+            $area_id = $authUser->seller->area_id;
+
+            $customers = Customer::where('area_id', $area_id)->pluck('user_id');
+            $filteredCustomers = User::whereIn('id', $customers)->where('joined_through', 'Seller')->get();
+
+            $lastcustomer = User::whereIn('id', $customers)->where('role', 'customer')->orderBy('id', 'desc')
+                ->take(5)->get();
+
+            $lastOrders = Order::whereIn('user_id', $customers)
+                ->select('id', 'uuid', 'cart_id', 'user_id', 'portal', 'status', 'created_at')
+                ->orderBy('id', 'desc')->take(5)->get();
+
+            $orders = Order::whereIn('user_id', $customers)->get();
+        } else {
+            $filteredCustomers = collect();
+            $lastcustomer = collect();
+            $lastOrders = collect();
+            $orders = collect();
+        }
+
+        $sellers = Seller::all();
+        $data['customers'] = [
+            'total' => $filteredCustomers->count(),
+            'verified' => $filteredCustomers->where('verified', 1)->count(),
+        ];
+
+        $data['sellers'] = [
+            'total' => $sellers->count(),
+            'verified' => $sellers->where('verified', 1)->count(),
+        ];
+
+        $data['orders'] = [
+            'total' => $orders->count(),
+            'pending' => $orders->where('status', 'Pending')->count(),
+            'verification' => $orders->where('status', 'Verification')->count(),
+            'processing' => $orders->where('status', 'Processing')->count(),
+            'delivered' => $orders->where('status', 'Delivered')->count(),
+            'instalments' => $orders->where('status', 'Instalments')->count(),
+            'Completed' => $orders->where('status','Completed')->count()
+        ];
+        $data['lastcustomer'] = $lastcustomer;
+        $data['lastOrders'] = $lastOrders;
+
+        return view('dashboards.sellers.index', $data);
     }
 
     /**
