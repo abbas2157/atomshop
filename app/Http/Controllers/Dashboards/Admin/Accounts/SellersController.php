7253b1523927c3fd17442cddc3b4f6fcb86d9b6f
Maryam Saleem <maryamsaleem108@gmail.com>
2025-03-15 00:44:37 +0500

    multiple area store when create seller

--- app/Http/Controllers/Dashboards/Admin/Accounts/SellersController.php
+++ app/Http/Controllers/Dashboards/Admin/Accounts/SellersController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Dashboards\Admin\Accounts;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\{Auth, Hash, DB};
-use App\Models\{User, Seller, City, Area};
+use App\Models\{User, Seller, City, Area, ActiveSeller};
 use Illuminate\Support\Str;
 
 class SellersController extends Controller
@@ -70,12 +70,22 @@ class SellersController extends Controller
                 $seller->cnic_number = $request->cnic_number;
                 $seller->website = $request->website;
                 $seller->city_id = $request->city_id;
-                $seller->area_id = $request->area_id;
+                // $seller->area_id = $request->area_id;
                 $seller->address = $request->business_address;
                 $seller->investment_capacity = $request->investment_capacity;
                 $seller->previous_experience = $request->previous_experience;
                 $seller->save();
 
+                if ($request->has('area_id')) {
+                    foreach ($request->area_id as $area_id) {
+                        ActiveSeller::create([
+                            'user_id' => $user->id,
+                            'area_id' => $area_id,
+                            'seller_id' => $seller->id,
+                            'status' => 'Active',
+                        ]);
+                    }
+                }
                 DB::commit();
 
                 $response = [ 'success' => true, 'message' => 'Sellers Added Successfully'];
@@ -86,6 +96,16 @@ class SellersController extends Controller
                 $response = [ 'success' => true, 'message' => $e->getMessage()];
                 return response()->json($response);
             }
+        }
+
+        public function getAreasByCity(Request $request)
+        {
+            $areas = Area::where('city_id', $request->city_id)
+                        ->where('status', 'active')
+                        ->orderBy('id', 'desc')
+                        ->get();
+
+            return response()->json($areas);
         }
 
     /**
--- resources/views/dashboards/admin/accounts/sellers/create-partials/business-address.blade.php
+++ resources/views/dashboards/admin/accounts/sellers/create-partials/business-address.blade.php
@@ -2,6 +2,11 @@
     <p class="mg-b-20"> To get registered, kindly fill out the form below with Seller's details</p>
     <div class="row row-sm">
         <div class="col-lg mt-2">
+            <label>Street Address <span class="text-danger">*</span></label>
+            <input type="text" class="form-control" name="business_address" id="business_address"
+                placeholder="Enter Street Address" value="{{ old('business_address') }}" required>
+        </div>
+        <div class="col-lg mt-2">
             <label>Seller city <span class="text-danger">*</span></label>
             <select class="form-control" name="city_id" id="city_id">
                 @if ($cities->isNotEmpty())
@@ -12,9 +17,11 @@
                 @endif
             </select>
         </div>
+    </div>
+    <div class="row row-sm">
         <div class="col-lg mt-2">
             <label>Seller area <span class="text-danger">*</span></label>
-            <select class="form-control select2" multiple name="area_id" id="area_id"
+            <select class="form-control select2" multiple name="area_id[]" id="area_id"
                 style="display: block; width:100%">
                 @if (!empty($areas))
                     @foreach ($areas as $index => $item)
@@ -23,13 +30,6 @@
                     @endforeach
                 @endif
             </select>
-        </div>
-    </div>
-    <div class="row row-sm">
-        <div class="col-lg mt-2">
-            <label>Street Address <span class="text-danger">*</span></label>
-            <input type="text" class="form-control" name="business_address" id="business_address"
-                placeholder="Enter Street Address" value="{{ old('business_address') }}" required>
         </div>
     </div>
 </section>
