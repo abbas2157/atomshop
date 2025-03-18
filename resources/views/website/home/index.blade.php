21380ca8833e5a380c051c7f87529c917f400b57
Mudassar Abbas <mudassar@zoomdigital.ae>
2025-03-18 12:16:13 +0500

    Home Page SEO Setup

--- resources/views/website/home/index.blade.php
+++ resources/views/website/home/index.blade.php
@@ -24,4 +24,30 @@
     </div>
     @include('website.home.partials.recent-products')
     @include('website.home.partials.brands')
+    <div class="container-fluid pt-2 pb-2">
+        <div class="row px-xl-5 mb-5">
+            <div class="col-12">
+                <div class="bg-light p-30">
+                    <div class="mb-3">
+                        <h1>Atomshop.pk</h1>
+                    </div>
+                    <div>
+                        <p>
+                            At <b>AtomShop.pk</b>, we make essential products <b>affordable and accessible </b> through <b>flexible installment plans</b>. 
+                            No one should have to delay their needs due to financial barriers—that’s why we provide easy payment 
+                            options for <b> appliances, electronics, and more</b>.
+                        </p>
+                        <p>
+                            But we go beyond just selling products—we <b>empower individuals</b> through <b>AtomShop SAAS</b>, 
+                            offering <b>training and digital tools</b> to help aspiring entrepreneurs <b>start their own businesses </b>
+                            and earn a sustainable income.
+                        </p>
+                        <p>
+                            <b>AtomShop.pk isn't just a marketplace—it's a platform for financial freedom and opportunity.</b>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/website/home/partials/featured-start.blade.php
+++ resources/views/website/home/partials/featured-start.blade.php
@@ -3,25 +3,25 @@
     <div class="row px-xl-5">
         <div class="col-lg-3 col-md-6 col-sm-12 pb-1">
             <div class="d-flex align-items-center bg-light mb-4 p-30" >
-                <h1 class="fa fa-receipt text-primary m-0 mr-3"></h1>
+                <span class="fa fa-receipt text-primary m-0 mr-3 fs-3"></span>
                 <h5 class="font-weight-semi-bold m-0">Custom Installment <br /> Plans</h5>
             </div>
         </div>
         <div class="col-lg-3 col-md-6 col-sm-12 pb-1">
             <div class="d-flex align-items-center bg-light mb-4 p-30" >
-                <h1 class="fa fa-phone-volume text-primary m-0 mr-2"></h1>
+                <span class="fa fa-phone-volume text-primary m-0 mr-2 fs-3"></span>
                 <h5 class="font-weight-semi-bold m-0">Easy Verification <br /> Process</h5>
             </div>
         </div>
         <div class="col-lg-3 col-md-6 col-sm-12 pb-1">
             <div class="d-flex align-items-center bg-light mb-4 p-30" >
-                <h1 class="fas fa-shipping-fast text-primary m-0 mr-3"></h1>
+                <span class="fas fa-shipping-fast text-primary m-0 mr-3 fs-3"></span>
                 <h5 class="font-weight-semi-bold m-0">Door-Step <br /> Services</h5>
             </div>
         </div>
         <div class="col-lg-3 col-md-6 col-sm-12 pb-1">
             <div class="d-flex align-items-center bg-light mb-4 p-30" >
-                <h1 class="fa fa-exchange-alt text-primary m-0 mr-3"></h1>
+                <span class="fa fa-exchange-alt text-primary m-0 mr-3 fs-3"></span>
                 <h5 class="font-weight-semi-bold m-0">Any Product, <br /> Sourced!</h5>
             </div>
         </div>
