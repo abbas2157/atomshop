79a139a3ea7be3cbebcfe2ae84c09b49380e4360
Mudassar Abbas <mudassar@zoomdigital.ae>
2025-03-19 15:21:55 +0500

    Login Page Setup

--- resources/views/website/auth/login.blade.php
+++ resources/views/website/auth/login.blade.php
@@ -6,35 +6,46 @@
 @endsection
 
 @section('content')
-<div class="container-fluid">
+<div class="container">
     <div class="row">
-        <div class="col-md-12">
+        <div class="col-md-5">
             <div class="login__signup__wrapper">
-                <div class="row d-flex align-items-center justify-content-center">
-                   <div class="col-lg-4">
-                        <form class="login-form" id="login-form" action="{{ route('login.perform') }}" autocomplete="off" onsubmit="return false;">
-                            @csrf
-                            <h4 class="heading">Login</h4>
-                            <p class="sub-text">Login to your atomshop.pk account</p>
-                            <div class="input-floating-label">
-                                <input class="input" type="text" id="email-input" name="email" placeholder="Enter email" autofocus autocomplete="off"/>
-                                <label for="email-input">Email</label>
-                                <span class="focus-bg"></span>
-                                <span class="text-danger error" id="error-email"></span>
-                            </div>
-                            <div class="input-floating-label">
-                                <input class="input" type="password" id="input-password" name="password" placeholder="password" autocomplete="new-password"/>
-                                <label for="input-password">Password</label>
-                                <span class="focus-bg"></span>
-                                <span class="text-danger error" id="error-password"></span>
-                            </div>
-                            <a href="" class="forgot__link">Forgot password?</a>
-                            <button class="btn__submit login" type="button">Login</button>
-                            <p class="other__link"> Don't have an account? <a href="{{ route('website.register') }}">Sign up</a>
-                            </p>
-                        </form>
+                <form class="login-form" id="login-form" action="{{ route('login.perform') }}" autocomplete="off" onsubmit="return false;">
+                    @csrf
+                    <h4 class="heading">Login</h4>
+                    <p class="sub-text">Login to your atomshop.pk account</p>
+                    <div class="input-floating-label">
+                        <input class="input" type="text" id="email-input" name="email" placeholder="Enter email" autofocus autocomplete="off"/>
+                        <label for="email-input">Email</label>
+                        <span class="focus-bg"></span>
+                        <span class="text-danger error" id="error-email"></span>
                     </div>
-                </div>
+                    <div class="input-floating-label">
+                        <input class="input" type="password" id="input-password" name="password" placeholder="password" autocomplete="new-password"/>
+                        <label for="input-password">Password</label>
+                        <span class="focus-bg"></span>
+                        <span class="text-danger error" id="error-password"></span>
+                    </div>
+                    <a href="" class="forgot__link">Forgot password?</a>
+                    <button class="btn__submit login" type="button">Login</button>
+                    <p class="other__link"> Don't have an account? <a href="{{ route('website.register') }}">Sign up</a>
+                    </p>
+                </form>
+            </div>
+        </div>
+        <div class="col-md-7">
+            <div class="bg-light px-5 py-4">
+                <h3>Sign In</h3>
+                <h5>Sign In to Your Atomshop.pk Account</h5>
+                <p>Welcome back to Atomshop.pk! Sign in to access your account, manage your installment plans, track your orders, and explore exclusive deals.</p>
+                <h5>Why Sign In?</h5>
+                <ul class="mb-5 pb-2">
+                    <li>View and manage your installment purchases.</li>
+                    <li>Track your order history and delivery status.</li>
+                    <li>Update your profile and payment details.</li>
+                    <li>Access exclusive discounts and offers.</li>
+                    <li>Get personalized recommendations based on your shopping preferences.</li>
+                </ul>
             </div>
         </div>
     </div>
