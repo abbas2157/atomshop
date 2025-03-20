29a0fd7e3e53a1ebfd5b05db17e2c072ef52a343
Mudassar Abbas <mudassar@zoomdigital.ae>
2025-03-20 10:29:26 +0500

    Google Tag

--- resources/views/others/google-analytics.blade.php
+++ resources/views/others/google-analytics.blade.php
@@ -1,16 +1,5 @@
 @if (App::environment('production'))
   <!-- Google tag (gtag.js) -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=G-KGMVWBDS0T"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'G-KGMVWBDS0T');
-  </script>
-  <!-- Google tag (gtag.js) -->
-
-  <!-- Google tag (gtag.js) -->
   <script async src="https://www.googletagmanager.com/gtag/js?id=G-SEETBSDDKQ"></script>
   <script>
     window.dataLayer = window.dataLayer || [];
